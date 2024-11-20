9d95720c606237ab4de4941f6c810709ecafb157
era97 <afrinjahanera03@gmail.com>
2024-11-20 15:09:10 +0600

    txt file of code smell

---
+++ Test/MainTest.java
@@ -0,0 +1,11 @@
+package Test;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+public class MainTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
